7e652d845ea30b53fa043a93af40ba4d2ce67852
Oludotun Ebiekuraju <dotun@intuitigo.com>
2023-05-13 12:11:57 +0100

    Added HTTPS support

--- index.js
+++ index.js
@@ -4,13 +4,38 @@
 
 // Dependencies
 var http = require("http");
-const { type } = require("os");
+var https = require("https");
 var url = require("url");
 var stringDecoder = require("string_decoder").StringDecoder;
 var config = require("./config");
+var fs = require("fs");
 
-// The server should respond to all requests with a string
-var server = http.createServer(function (req, res) {
+// Instantiating the HTTP server
+var httpServer = http.createServer(function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the server
+httpServer.listen(config.httpPort, function () {
+    console.log("The HTTP server is listening on port " + config.httpPort);
+});
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+    key: fs.readFileSync("./https/key.pem"),
+    cert: fs.readFileSync("./https/cert.pem"),
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+    console.log("The HTTPS server is listening on port " + config.httpsPort);
+});
+
+// All the server logic for both http and https server
+var unifiedServer = function (req, res) {
     // Get the URL and parse it
     var parseURL = url.parse(req.url, true);
 
@@ -71,18 +96,7 @@ var server = http.createServer(function (req, res) {
             console.log("Returning this response: ", statusCode, payloadString);
         });
     });
-});
-
-// Start the server
-server.listen(config.port, function () {
-    console.log(
-        "The server is listening on port " +
-            config.port +
-            " in " +
-            config.envName +
-            " mode"
-    );
-});
+};
 
 // Define the handlers
 var handlers = {};
